c81b891ba7d1b9bdb17fc7950c7052140248f14e
Matt Lyon <matthewlyon18@gmail.com>
2023-06-20 23:27:19 +0100

    Image recipe attribute

--- api/src/models/Image.ts
+++ api/src/models/Image.ts
@@ -1,13 +1,15 @@
-import { Schema, Document, model } from 'mongoose';
+import { Schema, Document, Types, model } from 'mongoose';
 
 export interface IImage extends Document {
     lowresPath?: string;
     fullresPath: string;
+    recipe: Types.ObjectId;
 }
 
 const imageSchema = new Schema<IImage>({
     lowresPath: { type: String },
     fullresPath: { type: String, required: true },
+    recipe: { type: Schema.Types.ObjectId, required: true, ref: 'Recipe' },
 });
 
 export const Image = model<IImage>('Image', imageSchema);
