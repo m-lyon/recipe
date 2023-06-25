9bf1220fb1fac8fdebd78a0df0f79b44eb505771
Matt Lyon <matthewlyon18@gmail.com>
2023-06-25 14:21:14 +0100

    updoots

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { CreateRecipePage } from './pages/CreateRecipePage';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-root.render(<div>Hello world</div>);
+root.render(
+    <ChakraProvider>
+        <CreateRecipePage />
+    </ChakraProvider>
+);
---
+++ client/src/pages/CreateRecipePage.tsx
@@ -0,0 +1,14 @@
+import { Container, Box } from '@chakra-ui/react';
+
+export function CreateRecipePage(props) {
+    return (
+        <Container maxW='2xl' centerContent>
+            <Box padding='4'>
+                There are many benefits to a joint design and development system. Not only does it
+                bring benefits to the design team, but it also brings benefits to engineering teams.
+                It makes sure that our experiences have a consistent look and feel, not just in our
+                design specs, but in production.
+            </Box>
+        </Container>
+    );
+}
