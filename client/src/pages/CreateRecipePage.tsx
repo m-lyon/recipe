dd69b912af0706400ddf22e6cb7e1eb6c9a359a3
Matt Lyon <matthewlyon18@gmail.com>
2023-06-25 16:10:00 +0100

    testing

--- client/src/pages/CreateRecipePage.tsx
+++ client/src/pages/CreateRecipePage.tsx
@@ -1,14 +1,30 @@
-import { Container, Box } from '@chakra-ui/react';
+import { Container, Box, SkeletonCircle, SkeletonText, Skeleton } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
+import { WithSubnavigation } from '../components/Navbar';
 
 export function CreateRecipePage(props) {
     return (
-        <Container maxW='2xl' centerContent>
-            <Box padding='4'>
-                There are many benefits to a joint design and development system. Not only does it
-                bring benefits to the design team, but it also brings benefits to engineering teams.
-                It makes sure that our experiences have a consistent look and feel, not just in our
-                design specs, but in production.
-            </Box>
-        </Container>
+        <>
+            <WithSubnavigation />
+            <Container maxW='container.xl'>
+                <Grid
+                    h='200px'
+                    templateRows='repeat(2, 1fr)'
+                    templateColumns='repeat(5, 1fr)'
+                    gap={4}
+                >
+                    <GridItem rowSpan={2} colSpan={1} bg='tomato' />
+                    <GridItem colSpan={2} bg='papayawhip' />
+                    <GridItem colSpan={2} bg='papayawhip' />
+                    <GridItem colSpan={4} bg='tomato' />
+                </Grid>
+            </Container>
+            {/* <Container maxW='2xl' centerContent>
+                <Box padding='4' boxShadow='lg' bg='white'>
+                    <SkeletonCircle size='10' />
+                    <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+                </Box>
+            </Container> */}
+        </>
     );
 }
