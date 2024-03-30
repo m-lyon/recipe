cd2cdd3b884176c640d91fbbf3f66b39a968334a
Matt Lyon <matthewlyon18@gmail.com>
2024-03-30 11:55:21 +0000

    update to eslintrc

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,72 +1,72 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  env: {
-    node: true,
-  },
-  plugins: ["@typescript-eslint", "prettier", "import"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-  ],
-  noInlineConfig: true,
-  rules: {
-    "prettier/prettier": [
-        "error",
-        {
-            singleQuote: true,
-            jsxSingleQuote: true,
-            printWidth: 100,
-            tabWidth: 4,
-        },
-    ],
-    "sort-imports": [
-      "error",
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
-        allowSeparatedGroups: true,
-      },
-    ],
-    "import/no-unresolved": "off",
-    "import/order": [
-      "warn",
-      {
-        pathGroups: [
-          {
-            "pattern": "@recipe/**",
-            "group": "external",
-            "position": "after",
-          }
-        ],
-        pathGroupsExcludedImportTypes: ["@recipe/**"],
-        groups: [
-          "builtin", // Built-in imports (come from NodeJS native) go first
-          "external", // <- External imports
-          "internal", // <- Absolute imports
-          ["sibling", "parent"], // <- Relative imports, the sibling and parent types they can be mingled together
-          "index", // <- index imports
-          "unknown", // <- unknown
-        ],
-        "newlines-between": "always",
-      },
-    ],
-
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project: "./tsconfig.json",
-      },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-  },
-  ignorePatterns: ["**/*.cjs"], // Exclude files ending in .cjs from linting
+    env: {
+        node: true,
+    },
+    plugins: ['@typescript-eslint', 'prettier', 'import'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+    ],
+    noInlineConfig: true,
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                jsxSingleQuote: true,
+                printWidth: 100,
+                tabWidth: 4,
+                trailingComma: 'es5',
+            },
+        ],
+        'sort-imports': [
+            'error',
+            {
+                ignoreCase: false,
+                ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+                allowSeparatedGroups: true,
+            },
+        ],
+        'import/no-unresolved': 'off',
+        'import/order': [
+            'warn',
+            {
+                pathGroups: [
+                    {
+                        pattern: '@recipe/**',
+                        group: 'external',
+                        position: 'after',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['@recipe/**'],
+                groups: [
+                    'builtin', // Built-in imports (come from NodeJS native) go first
+                    'external', // <- External imports
+                    'internal', // <- Absolute imports
+                    ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+                    'index', // <- index imports
+                    'unknown', // <- unknown
+                ],
+                'newlines-between': 'always',
+            },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+        },
+    },
+    ignorePatterns: ['**/*.cjs'], // Exclude files ending in .cjs from linting
 };
