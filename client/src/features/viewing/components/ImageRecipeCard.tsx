69fbeae395b94b79de43dfaa002a9116fd5e1224
Matt Lyon <matthewlyon18@gmail.com>
2024-09-27 17:51:14 +0100

    fix to spacing on cards in homepage when logged in

--- client/src/features/viewing/components/ImageRecipeCard.tsx
+++ client/src/features/viewing/components/ImageRecipeCard.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CloseIcon, EditIcon } from '@chakra-ui/icons';
-import { LinkBox, LinkOverlay, VStack } from '@chakra-ui/react';
+import { Box, Flex, LinkBox, LinkOverlay, VStack } from '@chakra-ui/react';
 import { Card, CardBody, CardHeader, Heading, IconButton, Spacer } from '@chakra-ui/react';
 
 import { ROOT_PATH } from '@recipe/constants';
@@ -22,35 +22,54 @@ export function ImageRecipeCard(props: Props) {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <LinkBox display='flex' flexDirection='column' alignItems='center'>
+        <LinkBox display='flex' flexDirection='column'>
             <Card
                 width='18rem'
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             >
-                {hasEditPermission ? (
-                    <IconButton
-                        variant='solid'
-                        colorScheme='gray'
-                        aria-label={`Edit ${recipe.title}`}
-                        icon={<EditIcon />}
-                        isRound={true}
-                        position='absolute'
-                        shadow='base'
-                        top='0'
-                        left='0'
-                        zIndex='1'
-                        opacity={{ base: 1, md: isHovered ? 1 : 0 }}
-                        transform={{
-                            base: 'translate(-50%, -50%)',
-                            md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
-                        }}
-                        transition='opacity 0.3s, transform 0.3s'
-                        width='1'
-                        as={Link}
-                        to={`${ROOT_PATH}/edit/recipe/${recipe.titleIdentifier}`}
-                    />
-                ) : null}
+                <Flex minWidth='max-content' direction='row'>
+                    <Box zIndex={1} display={hasEditPermission ? undefined : 'none'}>
+                        <Box position='absolute'>
+                            <IconButton
+                                variant='solid'
+                                colorScheme='gray'
+                                aria-label={`Edit ${recipe.title}`}
+                                icon={<EditIcon />}
+                                isRound={true}
+                                shadow='base'
+                                opacity={{ base: 1, md: isHovered ? 1 : 0 }}
+                                transform={{
+                                    base: 'translate(-50%, -50%)',
+                                    md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
+                                }}
+                                transition='opacity 0.3s, transform 0.3s'
+                                as={Link}
+                                to={`${ROOT_PATH}/edit/recipe/${recipe.titleIdentifier}`}
+                            />
+                        </Box>
+                    </Box>
+                    <Spacer />
+                    <Box zIndex={1} display={hasEditPermission ? undefined : 'none'}>
+                        <Box position='absolute'>
+                            <IconButton
+                                variant='solid'
+                                colorScheme='gray'
+                                aria-label={`Delete ${recipe.title}`}
+                                icon={<CloseIcon />}
+                                isRound={true}
+                                shadow='base'
+                                opacity={{ base: 1, md: isHovered ? 1 : 0 }}
+                                transform={{
+                                    base: 'translate(-50%, -50%)',
+                                    md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
+                                }}
+                                transition='opacity 0.3s, transform 0.3s'
+                                onClick={() => handleDelete(recipe._id)}
+                            />
+                        </Box>
+                    </Box>
+                </Flex>
                 <CardHeader>
                     <Heading size='md' color='blackAlpha.700'>
                         <LinkOverlay
@@ -84,27 +103,6 @@ export function ImageRecipeCard(props: Props) {
                         </VStack>
                     </LinkOverlay>
                 </CardBody>
-                {hasEditPermission ? (
-                    <IconButton
-                        variant='solid'
-                        colorScheme='gray'
-                        aria-label={`Delete ${recipe.title}`}
-                        icon={<CloseIcon />}
-                        isRound={true}
-                        position='absolute'
-                        shadow='base'
-                        top='0'
-                        right='-10'
-                        zIndex='1'
-                        opacity={{ base: 1, md: isHovered ? 1 : 0 }}
-                        transform={{
-                            base: 'translate(-50%, -50%)',
-                            md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
-                        }}
-                        transition='opacity 0.3s, transform 0.3s'
-                        onClick={() => handleDelete(recipe._id)}
-                    />
-                ) : null}
             </Card>
         </LinkBox>
     );
--- client/src/features/viewing/components/RecipeCard.tsx
+++ client/src/features/viewing/components/RecipeCard.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CloseIcon, EditIcon } from '@chakra-ui/icons';
-import { IconButton, LinkOverlay } from '@chakra-ui/react';
+import { Box, Flex, IconButton, LinkOverlay, Spacer } from '@chakra-ui/react';
 import { Card, CardBody, CardHeader, Heading, LinkBox } from '@chakra-ui/react';
 
 import { ROOT_PATH } from '@recipe/constants';
@@ -30,7 +30,7 @@ export function RecipeCard(props: Props) {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <LinkBox display='flex' flexDirection='column' alignItems='center'>
+        <LinkBox display='flex' flexDirection='column'>
             <Card
                 minH='10em'
                 width='18rem'
@@ -42,29 +42,48 @@ export function RecipeCard(props: Props) {
                     to={`${ROOT_PATH}/view/recipe/${recipe.titleIdentifier}`}
                     aria-label={`View ${recipe.title}`}
                 />
-                {hasEditPermission ? (
-                    <IconButton
-                        variant='solid'
-                        colorScheme='gray'
-                        aria-label={`Edit ${recipe.title}`}
-                        icon={<EditIcon />}
-                        isRound={true}
-                        position='absolute'
-                        shadow='base'
-                        top='0'
-                        left='0'
-                        zIndex='1'
-                        opacity={{ base: 1, md: isHovered ? 1 : 0 }}
-                        transform={{
-                            base: 'translate(-50%, -50%)',
-                            md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
-                        }}
-                        transition='opacity 0.3s, transform 0.3s'
-                        width='1'
-                        as={Link}
-                        to={`${ROOT_PATH}/edit/recipe/${recipe.titleIdentifier}`}
-                    />
-                ) : null}
+                <Flex minWidth='max-content' direction='row'>
+                    <Box zIndex={1} display={hasEditPermission ? undefined : 'none'}>
+                        <Box position='absolute'>
+                            <IconButton
+                                variant='solid'
+                                colorScheme='gray'
+                                aria-label={`Edit ${recipe.title}`}
+                                icon={<EditIcon />}
+                                isRound={true}
+                                shadow='base'
+                                opacity={{ base: 1, md: isHovered ? 1 : 0 }}
+                                transform={{
+                                    base: 'translate(-50%, -50%)',
+                                    md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
+                                }}
+                                transition='opacity 0.3s, transform 0.3s'
+                                as={Link}
+                                to={`${ROOT_PATH}/edit/recipe/${recipe.titleIdentifier}`}
+                            />
+                        </Box>
+                    </Box>
+                    <Spacer />
+                    <Box zIndex={1} display={hasEditPermission ? undefined : 'none'}>
+                        <Box position='absolute'>
+                            <IconButton
+                                variant='solid'
+                                colorScheme='gray'
+                                aria-label={`Delete ${recipe.title}`}
+                                icon={<CloseIcon />}
+                                isRound={true}
+                                shadow='base'
+                                opacity={{ base: 1, md: isHovered ? 1 : 0 }}
+                                transform={{
+                                    base: 'translate(-50%, -50%)',
+                                    md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
+                                }}
+                                transition='opacity 0.3s, transform 0.3s'
+                                onClick={() => handleDelete(recipe._id)}
+                            />
+                        </Box>
+                    </Box>
+                </Flex>
                 <CardHeader>
                     <Heading size='md' color='blackAlpha.700'>
                         {getCardTitle(recipe)}
@@ -77,27 +96,6 @@ export function RecipeCard(props: Props) {
                             .concat(recipe.calculatedTags.map((tag) => ({ value: tag! })))}
                     />
                 </CardBody>
-                {hasEditPermission ? (
-                    <IconButton
-                        variant='solid'
-                        colorScheme='gray'
-                        aria-label={`Delete ${recipe.title}`}
-                        icon={<CloseIcon />}
-                        isRound={true}
-                        position='absolute'
-                        shadow='base'
-                        top='0'
-                        right='-10'
-                        zIndex='1'
-                        opacity={{ base: 1, md: isHovered ? 1 : 0 }}
-                        transform={{
-                            base: 'translate(-50%, -50%)',
-                            md: `translate(-50%, -50%) scale(${isHovered ? 1 : 0})`,
-                        }}
-                        transition='opacity 0.3s, transform 0.3s'
-                        onClick={() => handleDelete(recipe._id)}
-                    />
-                ) : null}
             </Card>
         </LinkBox>
     );
