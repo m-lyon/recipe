ae261b9a0d375ef4f9d8290cd1495df00840ff3a
Matt Lyon <matthewlyon18@gmail.com>
2024-11-08 20:03:09 +0000

    removed redundant components and hooks
    Removed components:
    - EditableField
    - EditableItem
    
    Removed hooks:
    - useEditable
    - useItemList

--- client/src/common/components/index.tsx
+++ client/src/common/components/index.tsx
@@ -1,7 +1,5 @@
-export { EditableItem } from './EditableItem';
 export { DropdownItem } from './DropdownItem';
 export { ImageCarousel } from './ImageCarousel';
-export { EditableField } from './EditableField';
 export { CentredTextArea } from './CentredTextArea';
 export { EditableItemArea } from './EditableItemArea';
 export { FloatingLabelInput } from './FloatingLabelInput';
--- client/src/common/hooks/index.tsx
+++ client/src/common/hooks/index.tsx
@@ -1,8 +1,4 @@
-export { useItemList } from './useItemList';
-export { useEditable } from './useEditable';
 export { useErrorToast } from './useErrorToast';
 export { useWarningToast } from './useWarningToast';
 export { useSuccessToast } from './useSuccessToast';
 export { useNavigatableList } from './useNavigatableList';
-export type { UseItemListReturnType } from './useItemList';
-export type { UseEditableReturnType } from './useEditable';
