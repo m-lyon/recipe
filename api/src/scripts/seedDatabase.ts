7f6cc34e90e5d91bfc9dd911624a42c087748a08
Matt Lyon <matthewlyon18@gmail.com>
2026-03-29 21:59:22 +0100

    feat(api): add populateKeyPhrases seed data function with example key phrases

--- api/src/scripts/seedDatabase.ts
+++ api/src/scripts/seedDatabase.ts
@@ -2,11 +2,12 @@ import 'dotenv-flow/config';
 import mongoose from '../utils/database.js';
 import { populateIngredients, populateUsers } from '../utils/populate.js';
 import { populateImages, populateRecipes, populateTags } from '../utils/populate.js';
-import { populatePrepMethods, populateSizes, populateUnits } from '../utils/populate.js';
+import { populateKeyPhrases, populatePrepMethods, populateSizes, populateUnits } from '../utils/populate.js';
 
 async function populateData() {
     await populateUsers();
     await populateTags();
+    await populateKeyPhrases();
     await populateUnits();
     await populateSizes();
     await populateIngredients();
