0f5c669307e0c1297537fcd5243d1f8998cc5a6d
Matt Lyon <matthewlyon18@gmail.com>
2026-05-28 19:58:16 +0100

    fix

--- client/src/features/tags/utils/formatCalculatedTag.ts
+++ client/src/features/tags/utils/formatCalculatedTag.ts
@@ -1,8 +1,3 @@
-import { ReservedTags } from '@recipe/graphql/enums';
-
 export function formatCalculatedTag(tag: string) {
-    if (tag === ReservedTags.VeganVersionAvailable) {
-        return 'vegan version available';
-    }
-    return tag;
+    return tag.replace(/_/g, ' ');
 }
