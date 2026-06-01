9420d0ee8a55cf376d16979304fb976d3cf6ce67
Matt Lyon <matthewlyon18@gmail.com>
2026-06-01 21:28:49 +0100

    fix

--- client/src/__tests__/editing.update.test.ts
+++ client/src/__tests__/editing.update.test.ts
@@ -4,11 +4,11 @@ import { getCache } from '@recipe/utils/cache';
 import { ReservedTags } from '@recipe/graphql/enums';
 import { updateRecipeCache } from '@recipe/features/editing';
 import { GET_RECIPE, GET_RECIPES } from '@recipe/graphql/queries/recipe';
+import { mockRecipeThree } from '@recipe/graphql/queries/__mocks__/recipe';
 import { mockRecipeVeganCopy } from '@recipe/graphql/queries/__mocks__/recipe';
 import { archiveRecipeCache, deleteVeganRecipeCache } from '@recipe/features/editing';
 import { mockRecipeWithVeganVersion } from '@recipe/graphql/queries/__mocks__/recipe';
 import { mockRecipeFour, mockRecipeOne } from '@recipe/graphql/queries/__mocks__/recipe';
-import { mockRecipeThree, mockRecipeTwo } from '@recipe/graphql/queries/__mocks__/recipe';
 
 describe('editing cache helpers', () => {
     it('archiveRecipeCache removes the original from home-page lists, decrements count, and archives linked vegan recipe', () => {
@@ -24,14 +24,14 @@ describe('editing cache helpers', () => {
             },
             data: {
                 __typename: 'Query',
-                recipeMany: [mockRecipeWithVeganVersion, mockRecipeThree, mockRecipeFour],
+                recipeMany: [mockRecipeOne, mockRecipeWithVeganVersion, mockRecipeFour],
                 recipeCount: 3,
             },
         });
 
         cache.writeQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three' } },
             data: {
                 __typename: 'Query',
                 recipeOne: mockRecipeWithVeganVersion,
@@ -40,7 +40,7 @@ describe('editing cache helpers', () => {
 
         cache.writeQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one-vegan' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three-vegan' } },
             data: {
                 __typename: 'Query',
                 recipeOne: {
@@ -63,15 +63,15 @@ describe('editing cache helpers', () => {
         });
         const originalData = cache.readQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three' } },
         });
         const veganData = cache.readQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one-vegan' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three-vegan' } },
         });
 
         expect(homeData?.recipeMany.map((recipe) => recipe._id)).toEqual([
-            mockRecipeThree._id,
+            mockRecipeOne._id,
             mockRecipeFour._id,
         ]);
         expect(homeData?.recipeCount).toBe(2);
@@ -81,20 +81,6 @@ describe('editing cache helpers', () => {
 
     it('deleteVeganRecipeCache removes linked vegan state from the original and clears future vegan-copy reads', () => {
         const cache = getCache();
-        const originalWithLinkedTag = {
-            ...mockRecipeOne,
-            calculatedTags: [
-                ReservedTags.Vegan,
-                ReservedTags.Vegetarian,
-                'vegan version available',
-            ],
-            veganVersion: {
-                __typename: 'Recipe' as const,
-                _id: mockRecipeTwo._id,
-                title: mockRecipeVeganCopy.title,
-                titleIdentifier: mockRecipeVeganCopy.titleIdentifier,
-            },
-        };
 
         cache.writeQuery({
             query: GET_RECIPES,
@@ -106,23 +92,23 @@ describe('editing cache helpers', () => {
             },
             data: {
                 __typename: 'Query',
-                recipeMany: [originalWithLinkedTag, mockRecipeThree, mockRecipeFour],
+                recipeMany: [mockRecipeOne, mockRecipeWithVeganVersion, mockRecipeFour],
                 recipeCount: 3,
             },
         });
 
         cache.writeQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three' } },
             data: {
                 __typename: 'Query',
-                recipeOne: originalWithLinkedTag,
+                recipeOne: mockRecipeWithVeganVersion,
             },
         });
 
         cache.writeQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one-vegan' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three-vegan' } },
             data: {
                 __typename: 'Query',
                 recipeOne: mockRecipeVeganCopy,
@@ -142,11 +128,11 @@ describe('editing cache helpers', () => {
         });
         const originalData = cache.readQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three' } },
         });
         const veganData = cache.readQuery({
             query: GET_RECIPE,
-            variables: { filter: { titleIdentifier: 'mock-recipe-one-vegan' } },
+            variables: { filter: { titleIdentifier: 'mock-recipe-three-vegan' } },
         });
         expect(homeData?.recipeCount).toBe(3);
         expect(homeData?.recipeMany.map((recipe) => recipe._id)).toEqual([
